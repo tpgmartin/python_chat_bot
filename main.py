21da7212b2cece9f07f389ce122d9963b5e46792
tpgmartin <tpgmartin@gmail.com>
2018-01-11 15:07:06 +0000

    Setup project files

---
+++ main.py
@@ -0,0 +1,10 @@
+class ChatBot(object):
+
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
+
+    def get_response(self):
+        pass
---
+++ trainer.py
@@ -0,0 +1,7 @@
+class Trainer(object):
+
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
